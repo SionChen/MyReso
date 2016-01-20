62f62bfa48e2e399fd1179a11799fba3c844ea81
超级腕电商 <chaojiwandianshang@chaojiwandianshangdeimac.local>
2016-01-20 14:31:48 +0800

    11111

---
+++ yaoyiyao/yaoyiyao/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  yaoyiyao
+//
+//  Created by 晓 on 15/12/31.
+//  Copyright © 2015年 陈泳晓. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ yaoyiyao/yaoyiyao/ViewController.m
@@ -0,0 +1,68 @@
+//
+//  ViewController.m
+//  yaoyiyao
+//
+//  Created by 晓 on 15/12/31.
+//  Copyright © 2015年 陈泳晓. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    [[UIApplication sharedApplication] setApplicationSupportsShakeToEdit:YES];
+    
+    
+}
+
+
+- (void) motionBegan:(UIEventSubtype)motion withEvent:(UIEvent *)event
+
+{
+    NSLog(@"摇一摇");
+    //检测到摇动
+    
+}
+
+
+
+- (void) motionCancelled:(UIEventSubtype)motion withEvent:(UIEvent *)event
+
+{
+    NSLog(@"摇动取消");
+    
+    //摇动取消
+    
+}
+
+
+
+- (void) motionEnded:(UIEventSubtype)motion withEvent:(UIEvent *)event
+
+{
+    
+    //摇动结束
+    NSLog(@"摇动结束");
+    
+    if (event.subtype == UIEventSubtypeMotionShake) {
+        
+        //something happens
+        NSLog(@"这是什么玩意");
+        
+    }
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
